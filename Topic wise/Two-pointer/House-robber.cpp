40e8ec168d0319854e9829486ae14c6cf6e58684
Moxie-47 <prashants9700@gmail.com>
2025-05-28 12:21:07 +0530

    Tabulation way

--- Topic wise/Two-pointer/House-robber.cpp
+++ Topic wise/Two-pointer/House-robber.cpp
@@ -1,24 +1,19 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int Helper(const vector<int> &nums, int i, vector<int> &dp)
-{
-    if (i >= nums.size())
-    {
-        return 0;
-    }
-    if (dp[i] != -1)
-        return dp[i];
-    return dp[i] = max(nums[i] + Helper(nums, i + 2, dp),
-                       Helper(nums, i + 1, dp));
-}
-
 int rob(vector<int> &nums)
 {
     int n = nums.size();
+    if (n == 1)
+        return nums[0];
     vector<int> dp(n, -1);
-    Helper(nums, 0, dp);
-    return dp[0];
+    dp[0] = nums[0];
+    dp[1] = max(nums[0], nums[1]);
+    for (int i = 2; i < n; i++)
+    {
+        dp[i] = max(nums[i] + dp[i - 2], dp[i - 1]);
+    }
+    return dp[n - 1];
 }
 
 int main()
